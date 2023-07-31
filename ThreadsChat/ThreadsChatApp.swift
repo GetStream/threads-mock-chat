32e065532f3af0cd90c70424e32304b8f634fc03
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2023-07-31 13:58:42 +0200

    Initial Commit

---
+++ ThreadsChat/ThreadsChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsChatApp.swift
+//  ThreadsChat
+//
+//  Created by Jeroen Leenarts on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
